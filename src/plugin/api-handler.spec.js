abccf18f3fdfd0cdbccab731bd1b9417d0084b89
vouill <thibaud@sqreen.io>
2017-10-25 19:17:48 +0200

    added destroyed

--- src/plugin/api-handler.spec.js
+++ src/plugin/api-handler.spec.js
@@ -1,11 +1,19 @@
-// import Vue from 'vue/dist/vue.js'
+import Vue from 'vue/dist/vue.js'
 import { mount } from 'vue-test-utils'
 import ApiHandler from './api-handler'
 
 describe('Api handler', () => {
-  it('call the correct hooks when mounting and unmounting', () => {
+  it('call the correct hooks when mounting', () => {
     const dispatch = jest.fn()
     mount(ApiHandler, { mocks: { $store: { dispatch } }, propsData: { url: 'some/url', keyPath: 'totally-a-keypath' } })
+    expect(dispatch.mock.calls).toMatchSnapshot()
+  })
+
+  it('should be called after destroy', () => {
+    const dispatch = jest.fn()
+    const wrapper = mount(ApiHandler, { mocks: { $store: { dispatch } }, propsData: { persistent: false, url: 'some/url', keyPath: 'totally-a-keypath' } })
+    wrapper.vm.$destroy()
+    wrapper.update()
     expect(dispatch.mock.calls).toMatchSnapshot()
   })
 })
