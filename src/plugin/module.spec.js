c4bb5d85307bda5acff98aa12839442974f15e94
vouill <dev@vouill.com>
2017-12-15 18:29:21 +0100

    add test for onSuccess hook

--- src/plugin/module.spec.js
+++ src/plugin/module.spec.js
@@ -3,10 +3,18 @@
  */
 import module from './module'
 import pluginActions from './actions'
+import moxios from 'moxios'
 
 const testRequest = { url: 'testUrl', method: 'testMethod', baseURL: 'baseUrl' }
 
 describe('Module Actions', () => {
+  beforeEach(function () {
+    moxios.install()
+  })
+
+  afterEach(function () {
+    moxios.uninstall()
+  })
   it('should process request', () => {
     const commit = jest.fn()
     module.actions[pluginActions.request]({ commit }, testRequest)
@@ -15,8 +23,25 @@ describe('Module Actions', () => {
 
   it('should process clear', () => {
     const commit = jest.fn()
-    module.actions[pluginActions.clear]({ commit }, 'someKeypath')
+    module.actions[pluginActions.clear]({ commit }, { keyPath: 'someKeypath' })
     expect(commit.mock.calls).toMatchSnapshot()
+  })
+
+  it('should process onSuccess', () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: 'test'
+      })
+    })
+
+    const commit = jest.fn()
+    const dispatch = jest.fn()
+    return module.actions[pluginActions.request]({ commit, dispatch }, { keyPath: 'someKeypath', url: 'testUrl', onSuccess: { dispatchAction: { type: 'test-type', payload: 'test-payload' } } }).then(() => {
+      expect(commit.mock.calls).toMatchSnapshot()
+      expect(dispatch.mock.calls).toMatchSnapshot()
+    })
   })
 })
 
