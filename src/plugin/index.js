e0aa1a9ee5859006ebd6172c3a64f5d08b33fb16
vouill <dev@vouill.com>
2018-07-07 23:46:35 +0200

    expose hoc

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,5 +2,5 @@ import { default as module } from './module'
 export { default as actions } from './actions'
 export { ApiHandlerComponent } from './api-handler-component'
 export { getApiResp, getApiState, getApiStatus, getApiData } from './api-handler-helper'
-export { default as vuexApiHoc } from './vuex-api-hoc'
+export { default as apiHoc } from './vuex-api-hoc'
 export default module
