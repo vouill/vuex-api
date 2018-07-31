550cc687f8a2d63e8df68792f8bc905f1dd4f2b3
vouill <dev@vouill.com>
2018-07-31 15:15:11 +0200

    improved hoc conditions

--- src/plugin/vuex-api-hoc.js
+++ src/plugin/vuex-api-hoc.js
@@ -15,18 +15,24 @@ export const ApiHandlerComponent = {
     if(!this.storeObject){
       return null
     }
-    if(this.storeObject.status === 'loading'){
-      this.$slots.loading[0].componentOptions.propsData.storeObject = this.storeObject
+    if(this.storeObject.status === 'loading'  && this.$slots.loading){
+      if(this.$slots.loading[0].componentOptions){
+        this.$slots.loading[0].componentOptions.propsData.vuexApiStoreObject = this.storeObject
+      }
       return h('div', this.$slots.loading)
     }
     if(this.storeObject.status === 'success' && this.$slots.success){
-      this.$slots.success[0].componentOptions.propsData.storeObject = this.storeObject
-      this.$slots.success[0].componentOptions.propsData.vuexApiData = this.storeObject.resp.data
+      if(this.$slots.success[0].componentOptions){
+        this.$slots.success[0].componentOptions.propsData.vuexApiStoreObject = this.storeObject
+        this.$slots.success[0].componentOptions.propsData.vuexApiData = this.storeObject.resp.data
+      }
       return h('div', this.$slots.success)
     }
-    if(this.storeObject.status === 'error'){
-      this.$slots.error[0].componentOptions.propsData.storeObject = this.storeObject
-      this.$slots.error[0].componentOptions.propsData.vuexApiError = this.storeObject.resp.err
+    if(this.storeObject.status === 'error' && this.$slots.error){
+      if(this.$slots.error[0].componentOptions){
+        this.$slots.error[0].componentOptions.propsData.vuexApiStoreObject = this.storeObject
+        this.$slots.error[0].componentOptions.propsData.vuexApiError = this.storeObject.resp.err
+      }
       return h('div', this.$slots.error)
     }
     return null
